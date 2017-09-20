a5878f6426c05ff94a6336e9c382861658dc3724
SiuKit Wong <skwongr@gmail.com>
2017-09-20 15:35:46 -0500

    add nested addon/engines support

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+'use strict';
+
+// For ember-cli < 2.7 findHost doesnt exist so we backport from that version
+// for earlier version of ember-cli.
+//https://github.com/ember-cli/ember-cli/blame/16e4492c9ebf3348eb0f31df17215810674dbdf6/lib/models/addon.js#L533
+function findHostShim() {
+  let current = this;
+  let app;
+  do {
+    app = current.app || app;
+  } while (current.parent.parent && (current = current.parent));
+  return app;
+}
+
 module.exports = {
   name: 'ember-select-2',
 
-  included: function(app) {
-    this._super.included(app);
- 
+  included: function(appOrAddon) {
+    let findHost = this._findHost || findHostShim;
+    let app = findHost.call(this);
+
+    this._super.included.apply(this, arguments);
+
     app.import(app.bowerDirectory + '/select2/select2.js');
     app.import(app.bowerDirectory + '/select2/select2.css');
     app.import(app.bowerDirectory + '/select2/select2.png', { destDir: 'assets' });
